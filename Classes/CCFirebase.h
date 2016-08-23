42133b6bfd8094022afdcbb00af5f25779fd363b
yalab <rudeboyjet@gmail.com>
2016-08-23 16:39:41 +0900

    First commit.

---
+++ Classes/CCFirebase.h
@@ -0,0 +1,18 @@
+//
+//  CCFirebase.h
+//  
+//
+//  Created by Atsushi Yoshida on 2016/08/23.
+//
+//
+
+#ifndef CCFirebase_h
+#define CCFirebase_h
+
+namespace oke_ya;
+
+class CCFirebase
+{
+};
+
+#endif /* CCFirebase_h */
